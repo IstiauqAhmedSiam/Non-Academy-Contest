e5653b86c63132ee4175c0c9c22b0c792d6fa752
Istiauq Ahmed Siam <72971888+IstiauqAhmedSiam@users.noreply.github.com>
2023-08-04 05:16:34 +0600

    Add files via upload

---
+++ Content Aug 2023/Project 2/script.js
@@ -0,0 +1,4 @@
+// write your JavaScript here
+document.querySelector(".menu_toggle_icon").addEventListener("click", function(){
+    document.querySelector("html").classList.toggle("menu_open");
+})
